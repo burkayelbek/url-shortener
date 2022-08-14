eaf47e4045f50f3db2d2a1b40d6649c251d61c24
burkayelbek <burkay.elbek@gmail.com>
2022-08-15 00:00:52 +0300

    URLShortener model created

--- urlshortener/admin.py
+++ urlshortener/admin.py
@@ -1,3 +1,11 @@
+from re import search
 from django.contrib import admin
+from urlshortener.models import Shortener
 
-# Register your models here.
+
+# admin.site.register(Shortener)
+
+@admin.register(Shortener)
+class ShortenerAdmin(admin.ModelAdmin):
+    search_fields = ('long_url','short_url')
+    list_display= ('long_url','short_url')
