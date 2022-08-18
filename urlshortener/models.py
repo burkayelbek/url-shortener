a04ed27c70122cae6bd4481e5070a69b0454d666
burkayelbek <burkay.elbek@gmail.com>
2022-08-18 17:54:23 +0300

    Url Shortener form and views implemented

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .utils import create_shortened_url
 
 class Shortener(models.Model):
 
@@ -16,3 +17,8 @@ class Shortener(models.Model):
     def __str__(self):
         return f'{self.long_url} to {self.short_url}' # That shows db data properly.
         
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+        # We pass the model instance that is being saved
+            self.short_url = create_shortened_url(self)
+        super().save(*args, **kwargs)
